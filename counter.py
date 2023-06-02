505ecc4468bd8d83c44d85567b0bbd077f4648ce
Matt Wyatt <matt.wyatt@veteransunited.com>
2023-06-02 13:01:20 -0500

    1

--- counter.py
+++ counter.py
@@ -3,7 +3,20 @@ def countEmail():
     name = input("Enter file:")
     if len(name) < 1 : name = "mbox-short.txt"
     handle = open(name)
-        
+    senders = dict()
+    for line in handle:
+        if line.startswith("From "):
+            words = line.split()
+            senders[words[1]] = senders.get(words[1],0) +1
+    big = -1
+    bigsender = None
+    for k,v in senders.items():
+        if v > big:
+            big = v
+            bigsender = k
+    print(bigsender,big)
+   
+
 
 ## if you want to test locally run > python counter.py
 if __name__ == "__main__":
